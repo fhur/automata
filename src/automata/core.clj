5fbdea58cee530f7c3d4589e56e5519b3bff3a29
fhur <fernando@authy.com>
2015-06-14 22:52:22 -0500

    Added documentation

--- src/automata/core.clj
+++ src/automata/core.clj
@@ -1,7 +1,14 @@
 (ns automata.core)
 
 (defn create-dfa
+  "Creates a hashmap based representation of a DFA.
+  Syntax: (create-dfa init-state accept-states & transitions)
+  Where each transition is a 3-tuple of ['initial state' 'input' 'resulting state']
+  Note: this method does no validation on the transitions."
   [init-state accept-states & transitions ]
+  {:pre [(coll? accept-states)
+         (not (empty? transitions))
+         (not (empty? accept-states))]}
   {:init init-state
    :accept (set accept-states)
    :transitions (reduce (fn [dfa-map current]
@@ -16,6 +23,9 @@
              state))
 
 (defn eval-dfa
+  "Executes the given DFA as created by create-dfa using the given
+  string as input. The method returns true if both the string finished
+  and the automata resulted in an accepting state"
   [dfa string]
   (accept? dfa
     (reduce (fn [state input]
