c5de089aedf7ad2809638b910af92aba95cdcff6
fhur <fernando@authy.com>
2015-06-17 09:31:55 -0500

    Added tests to empty, concat and or NFAs

--- test/automata/core_test.clj
+++ test/automata/core_test.clj
@@ -103,4 +103,52 @@
   :when ["1111000000000000000000000101010101011"] = true
   :when [ (apply str (repeat 500 "10101")) ] = true) ;; this tests that there is no stack overflow for large string
 
+(def nfa-empty-string (create-nfa :a [:b] [:a :eps :b]))
+
+;; A NFA for the "" regex
+(expected-when "Empty string NFA should only accept the empty string" (partial eval-nfa nfa-empty-string)
+  :when [""] = true
+  :when [" "] = false
+  :when ["a"] = false
+  :when ["ba"] = false
+  :when ["c"] = false
+  :when ["dasdf"] = false
+  :when ["1234"] = false)
+
+(let [single-char-nfa (create-nfa :a [:b] [:a "a" :b])]
+  (expected-when "A NFA that accepts only the 'a' string" (partial eval-nfa single-char-nfa)
+    :when [""] = false
+    :when [" "] = false
+    :when ["a"] = true
+    :when ["ab"] = false
+    :when ["a "] = false
+    :when ["ba"] = false
+    :when ["b"] = false))
+
+(let [single-char-concat-nfa (create-nfa :a [:end] [:a "a" :b]
+                                                   [:b "b" :c]
+                                                   [:c "c" :end])
+      exec-nfa (partial eval-nfa single-char-concat-nfa)]
+  (expected-when "An NFA that accepts the 'abc' concat" exec-nfa
+    :when [""] = false
+    :when [" "] = false
+    :when ["a"] = false
+    :when ["ab"] = false
+    :when ["abc"] = true
+    :when ["abcd"] = false))
+
+(let [or-nfa (create-nfa :a [:end] [:a :eps :b] [:a :eps :c]
+                                   [:b "1" :d]
+                                   [:c "0" :e]
+                                   [:d :eps :end]
+                                   [:e :eps :end])
+      exec-nfa (partial eval-nfa or-nfa)]
+  (expected-when "An NFA that accepts either 0 or 1 but not both" exec-nfa
+    :when [""] = false
+    :when ["0"] = true
+    :when ["1"] = true
+    :when [" "] = false
+    :when ["10"] = false
+    :when ["01"] = false))
+
 
