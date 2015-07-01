ae21457cc06e925e3de560dd9797e6b2133a8106
fhur <fernando@authy.com>
2015-06-30 22:54:06 -0500

    Implemented algorithm to convert from NFA to DFA and execute DFAs

--- src/automata/core.clj
+++ src/automata/core.clj
@@ -5,6 +5,13 @@
                       (do (alter counter + 1)
                           cur-val))))))
 
+(defn conj-all
+  "Conjoins all the items in coll 2 to coll"
+  [coll coll2]
+  (if (empty? coll2)
+    coll
+    (apply (partial conj coll) coll2)))
+
 (defn in?
   "true if seq contains elm"
   [coll elm]
@@ -16,12 +23,21 @@
   [nfa state input]
   (get (:transitions nfa) [state input] []))
 
+(defn transition*
+  [nfa states input]
+  (mapcat #(transition nfa % input) states))
+
 (defn e-clos
   "A list of all states including the given state that are
   accesible via epsilon moves"
   [nfa state]
   (cons state
         (mapcat (partial e-clos nfa) (transition nfa state :eps))))
+
+(defn e-clos-transition
+  [nfa states input]
+  (mapcat (partial e-clos nfa)
+          (transition* nfa states input)))
 
 (defn accepting-inputs
   "Returns the list of inputs that a list of states accepts
@@ -117,4 +133,66 @@
                [(:end nfa) :eps :kleen-init]
                [(:end nfa) :eps :kleen-end])))
 
+(defn dfa-init [nfa]
+  (e-clos nfa (:init nfa)))
 
+(defn dfa-end [nfa dfa-transitions]
+  (reduce (fn [end-states [_ _ out-state]]
+            (if (in? out-state (:end nfa))
+              (conj end-states out-state)
+              end-states))
+          #{} dfa-transitions))
+
+(defn- dfa-transitions
+  "Step function in the process of converting an NFA to a DFA."
+  [nfa states]
+  (map (fn [input]
+         [states input
+          (e-clos-transition nfa states input)])
+       (accepting-inputs nfa states)))
+
+(defn new-transitions
+  "Given a set of transitions a.k.a. transitions-set and a
+  collection of transition this method will return all
+  transitions that are in next-trns and not in transitions-set"
+  [transitions-set next-trns]
+  (clojure.set/difference (set next-trns) transitions-set))
+
+(defn transitions->out-states
+  "Given a list of transitions, this method returns the
+  out state of each transition. Every transition is a
+  [in-state input out-state] triple"
+  [transitions]
+  (set (map last transitions)))
+
+(defn transitions:nfa->dfa
+  [nfa]
+  (loop [queue [(dfa-init nfa)]
+         transitions #{}]
+    (if (empty? queue)
+      transitions
+      (let [next-trns (dfa-transitions nfa (first queue))
+            diff-trns (new-transitions transitions next-trns)]
+        (recur (conj-all (rest queue)
+                         (transitions->out-states diff-trns))
+               (conj-all transitions next-trns))))))
+
+(defn nfa->dfa
+  [nfa]
+  (let [transitions (transitions:nfa->dfa nfa)]
+    {:init (dfa-init nfa)
+     :end (dfa-end nfa transitions)
+     :transition-list transitions
+     :transitions (reduce (fn [trns [in state out]]
+                            (assoc trns [in state] out))
+                          {} transitions)}))
+
+(defn exec-dfa
+  [dfa string]
+  (loop [string string
+         state (:init dfa)]
+    (cond
+      (and (in? (:end dfa) state) (empty? string)) true
+      (or (empty? string) (nil? state)) false
+      :else (recur (.substring string 1)
+                   (get (:transitions dfa) [state (.substring string 0 1)])))))
